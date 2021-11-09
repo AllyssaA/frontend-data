0dad95d921e82e71af7f704924dca6335ad03f12
Allyssa <a.alimoestar@gmail.com>
2021-11-09 21:06:23 +0100

    corrected function-data

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 
-
+// https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'))
   })
