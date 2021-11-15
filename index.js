16d7b36200d45178d7bffb183caf3be5905ef261
Allyssa <a.alimoestar@gmail.com>
2021-11-15 21:59:44 +0100

    tutorial adding title

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const svg = d3.select('body').append('svg')
 
 // Group used to enforce margin
 const g = svg.append('g')
-.attr('transform', `translate(${margin.left},${margin.top})`);
+  .attr('transform', `translate(${margin.left},${margin.top})`
+  )
 
 
+// Global variable for all data
+const data = [66.38, 21.51, 23.37, 34.17, 36.21];
 const bar_height = 50;
+
 
 
 d3.json('http://hp-api.herokuapp.com/api/characters').then((json) => {
@@ -20,6 +24,23 @@ d3.json('http://hp-api.herokuapp.com/api/characters').then((json) => {
 
   update(data)
 })
+
+
+// DATA JOIN
+const rect = g.selectAll('rect').data(data).join(
+  //enter new elements
+  (enter) => enter.append('rect').attr('x', 0),
+  // update existing elements
+  (update) => update,
+  // elements that aren't associated with data
+  (exit) => exit.remove()
+)
+
+
+rect
+  .attr('height', bar_height)
+  .attr('width', (d) => d * 7)
+  .attr('y', (d, i) => i * (bar_height + 5))
 
 // fetch(`http://hp-api.herokuapp.com/api/characters`).then( 
 //   x => {
