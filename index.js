8a61b44985280d74b16808b1097f959ca3b2c2ae
Allyssa <a.alimoestar@gmail.com>
2021-11-15 21:50:53 +0100

    tutorial continues

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-console.log('hello world')
+const margin = {top: 40, bottom: 10, left: 120, right: 20};
+const width = 800 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
 
-const margin = {top: 40, bottom: 10, left: 120, right: 20}
-const width = 800 - margin.left - margin.right
-const height = 600 - margin.top - margin.bottom
-
-// Create svg element
+// Creates sources <svg> element
 const svg = d3.select('body').append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
+.attr('width', width+margin.left+margin.right)
+.attr('height', height+margin.top+margin.bottom);
+
+// Group used to enforce margin
+const g = svg.append('g')
+.attr('transform', `translate(${margin.left},${margin.top})`);
 
 
-  const g = svg.append('g')
-    .attr('transform', 'translate(${margin.left}, ${margin.top})')
+const bar_height = 50;
 
-  // global var for data
-  const dataSet = [66.38, 21.51, 23.37, 34.17, 36.21]
 
-  const barHeight = 50
+d3.json('http://hp-api.herokuapp.com/api/characters').then((json) => {
+  data = json
 
-// data join
-const rect = g.selectAll('rect').data(dataSet).join(
-  //new element
-  (enter) => enter.append('rect').attr('x', 0),
-  // update existing elements
-  (update) => update,
-  // elements that ren't associated with data
-  (exit) => exit.remove()
-)
+  update(data)
+})
 
-// Enter and update old and new elements
-rect
-  .attr('height', barHeight)
-  .attr('width', (d) => d * 7)
-  .attr('y', (d, i) => i * (barHeight + 5))
+// fetch(`http://hp-api.herokuapp.com/api/characters`).then( 
+//   x => {
+//     return x.json()
+//   }
+// )
+//   .then(
+//     c => {
+//     console.log(c)
+  
+//   }
+// )
 
-  rect.select('title').text((d) => `value: ${d}`)
+
+
+
+
+
+
